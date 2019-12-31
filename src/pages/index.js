e1a6e9776ab316b988dcc7b410bc7daf7bf4dadd
bizkin <litvaktal04@gmail.com>
2019-12-31 02:45:48 +0200

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" />
+    <h3>This is my sketches i don't share on social media</h3>
+    <h4>little about me, 28 old, got ambitions to make the world a better place ;) and...</h4>
     <p>Looking to enter the media/products management.</p>
     <p>feel free to <Link style={{ color: 'red' }} to="/contact">contact</Link> me</p>
     <Image />
