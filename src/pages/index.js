8dd52d8bf98a5fb255677cec9071d57736bffef3
bizkin <litvaktal04@gmail.com>
2020-01-02 05:50:53 +0200

    good commit baby

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio" />
-    <h3>This is my sketches i don't share on social media</h3>
-    <h4>little about me, 28 old, got ambitions to make the world a better place ;) and...</h4>
-    <p>Looking to enter the media/products management.</p>
+    <h3 style={{ color: 'rgb(160, 168, 255)' }}>This is my sketches i don't share on social media</h3>
+    <h5>About me?</h5>
+
+    <h4>IL BASED, {new Date().getFullYear() - 1992} old, got ambition to publish at least one book <a href="https://blueflame.netlify.com/">"Blue Flame"</a></h4>
+    <br /> and also ...
+    <p>I'm Looking to enter the media/products management:</p>
+    <p>as <strong>artist/compositor/creator/campaigner/director</strong></p>
     <p>feel free to <Link style={{ color: 'red' }} to="/contact">contact</Link> me</p>
     <Image />
     <Link to="/contact/">contact?</Link>
